1a1ef9dfe4d89c777a2c740771db4ea151eb901f
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-12-15 11:17:06 -0300

    avance 15-12 prod mode

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors({
     // en modo produccion
-    // origin: 'https://transcurrin-cl-client.vercel.app',
+    origin: 'https://transcurrin-cl-client.vercel.app',
 
 
 // tambien cambiar en el vercel.json
@@ -19,10 +19,10 @@ app.use(cors({
 
 
     // en modo dev
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    // origin: 'http://localhost:5173',
+    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
+    // credentials: true
 }))
 
 
