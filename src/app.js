541f2439e01b280774e9694cdd25af47ad3318ad
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-12-15 11:38:51 -0300

    trying resolve 15-12

--- src/app.js
+++ src/app.js
@@ -16,11 +16,6 @@ app.use(cors({
         // en modo dev
     // origin: 'http://localhost:5173',
 
-
-
-
-
-
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
