5afc80383f363f7ca5c154d7006569c5cc0857a2
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2023-12-15 11:44:10 -0300

    trying resolve getServices

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -3,7 +3,7 @@ import { TOKEN_SECRET } from "../config.js"
 
 export const authRequired = (req, res, next) => {
     const { token } = req.cookies
-    if (!token) return res.status(401).json({ message: "Autorizacion denegada" })
+    if (!token) return res.status(401).json({ message: "Autorizacion denegada token no valido" })
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
         if (err) return res.status(403).json({ message: "Token invalidos" })
         req.user=user
