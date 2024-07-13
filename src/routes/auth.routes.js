fe5f993b8afaf749e5299c3dcb9f30f983de823c
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2024-07-13 03:32:32 -0400

    fix routes auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 const router = Router()
-import {login,register,logout,profile,verifyToken,sendMail, sendMailReset} from "../controllers/auth.controller.js"
+import {login,loginApp, register,logout,profile,verifyToken,sendMail, sendMailReset} from "../controllers/auth.controller.js"
 import { authRequired } from "../middlewares/validateToken.js";
 import { validateSchema} from "../middlewares/validator.middleware.js"
 import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
 router.post("/register", validateSchema(registerSchema), register)
 router.post("/login", login)
-router.post("/loginApp", login)
+router.post("/loginApp", loginApp)
 
 
 router.post("/logout", logout)
